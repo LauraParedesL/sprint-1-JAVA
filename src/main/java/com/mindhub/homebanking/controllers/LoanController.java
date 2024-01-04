b084d74b635c6b78b3803239a5441928725c2624
laura <lauraparedesl35@outlook.com>
2024-01-04 16:24:16 -0500

    commit2

--- src/main/java/com/mindhub/homebanking/controllers/LoanController.java
+++ src/main/java/com/mindhub/homebanking/controllers/LoanController.java
@@ -1,14 +1,16 @@
 package com.mindhub.homebanking.controllers;
 
+import com.mindhub.homebanking.dto.LoanApplicationDTO;
 import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.models.ClientLoan;
+import com.mindhub.homebanking.models.Loan;
 import com.mindhub.homebanking.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -27,8 +29,41 @@ public class LoanController {
 
     @Transactional
     @PostMapping("/loans")
-    public ResponseEntity<String> loanRequest(@RequestParam String name,
-                                              @RequestParam String originAccount,
-                                              @RequestParam Double amount,
-                                              @RequestParam Integer payments)
+    public ResponseEntity<Object> loanRequest(
+            @RequestBody LoanApplicationDTO loanApplicationDTO,
+                            Authentication authentication){
+
+        Loan loanSelected = loanRepository.findById(loanApplicationDTO.getLoanId()).orElse(null);
+        Client client = clientRepository.findByEmail(authentication.getName());
+
+        if (loanApplicationDTO.getNumber().isBlank()) {
+            return new ResponseEntity<>("Missing data", HttpStatus.FORBIDDEN);
+        }
+        if (loanApplicationDTO.getLoanId() == null) {
+            return new ResponseEntity<>("not found", HttpStatus.FORBIDDEN);
+        }
+        if (loanApplicationDTO.getAmount() <= 0) {
+            return new ResponseEntity<>("Missing data", HttpStatus.FORBIDDEN);
+        }
+        if (loanApplicationDTO.getPayments() <= 0) {
+            return new ResponseEntity<>("Missing data", HttpStatus.FORBIDDEN);
+        }
+        if (!loanRepository.existsById(loanApplicationDTO.getLoanId())) {
+            return new ResponseEntity<>("Not loan found", HttpStatus.FORBIDDEN);
+        }
+        if (loanApplicationDTO.getAmount() > loanSelected.getMaxAmount()) {
+            return new ResponseEntity<>("Exceeds the maximum amount allowed for loan", HttpStatus.FORBIDDEN);
+        }
+        if (!loanSelected.getPayments().contains(loanApplicationDTO.getPayments())) {
+            return new ResponseEntity<>("Error: the payment selected is wrong", HttpStatus.FORBIDDEN);
+        }
+        if (!accountRepository.existsByNumber(loanApplicationDTO.getNumber())){
+            return new ResponseEntity<>("Error: the destination account doesn't exist", HttpStatus.FORBIDDEN);
+        }
+        if (client.getAccounts().contains(loanApplicationDTO.getNumber())){
+            return new ResponseEntity<>("Error: not match found", HttpStatus.FORBIDDEN);
+        }
+
+        ClientLoan clientLoan = new ClientLoan(amount * % / 100 , )
+    }
 }
--- src/main/java/com/mindhub/homebanking/dto/LoanApplicationDTO.java
+++ src/main/java/com/mindhub/homebanking/dto/LoanApplicationDTO.java
@@ -1,13 +1,13 @@
 package com.mindhub.homebanking.dto;
 
 public class LoanApplicationDTO {
-    private Long id;
+    private Long loanId;
     private Double amount;
     private Integer payments;
     private String number;
 
-    public Long getId() {
-        return id;
+    public Long getLoanId() {
+        return loanId;
     }
 
     public Double getAmount() {
@@ -20,5 +20,12 @@ public class LoanApplicationDTO {
 
     public String getNumber() {
         return number;
+    }
+
+    public LoanApplicationDTO(Long loanId, Double amount, Integer payments, String number) {
+        this.loanId = loanId;
+        this.amount = amount;
+        this.payments = payments;
+        this.number = number;
     }
 }
